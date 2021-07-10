aa7ba648911b89b856b717ba714eea2565fae119
Cezar Rosa <cecez86@gmail.com>
2021-07-10 14:30:37 -0300

    - receiving POST data (using body-parser dependency)

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,12 @@
 module.exports = (app) => {
     app.get('/atendimentos', (request, response) => {
-        console.log('/atendimentos acessado') 
+        console.log('GET /atendimentos acessado') 
         response.send('Oi, aqui é do servidor e você está na rota de atendimentos, volte em breve.') 
+    })
+
+    app.post('/atendimentos', (request, response) => {
+        console.log('POST /atendimentos acessado, dados da requisição:') 
+        console.log(request.body)
+        response.send('Oi, aqui é do servidor e você está POSTando na rota de atendimentos, grato.') 
     })
 }
